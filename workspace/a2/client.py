36fd7e9c1f6e980e7f883193b75bfa366a917d19
Andrea Mildes <Andrea Mildes>
2016-11-08 17:41:32 +0100

    implement client and server

--- workspace/a2/client.py
+++ workspace/a2/client.py
@@ -1,6 +1,16 @@
 import socket
+import json
 
 s = socket.socket()
-s.bind(localhost, 8080)
+s.connect(('localhost', 8080))
 
-s.listen(5)
+name = input('Please insert name: ')
+age = input('Please insert age: ')
+mat = input('Please insert matrikelnumber: ')
+
+jsonObj = {'Name': name, 'Age': age, 'Matrikelnummer': mat}
+jsonStr = json.dumps(jsonObj)
+
+s.send(jsonStr.encode())
+
+s.close()
--- workspace/a2/server.py
+++ workspace/a2/server.py
@@ -1,6 +1,16 @@
 import socket
+import json
 
 s = socket.socket()
-s.bind(localhost, 8080)
+s.bind(('localhost', 8080))
 
 s.listen(5)
+
+while True:
+	c, addr = s.accept()
+	jsonData = c.recv(1024)
+	jsonObj = json.loads(jsonData.decode())
+	print('Name: ', jsonObj['Name'])
+	print('Age: ', jsonObj['Age'])
+	print('Matrikelnummer: ', jsonObj['Matrikelnummer'])
+	c.close()
