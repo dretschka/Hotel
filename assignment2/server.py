b23960cc1cc24f71e4231b34e5716029d4aa4001
Andrea Mildes <Andrea Mildes>
2016-11-08 21:36:37 +0100

    add groupnames to assignment code add screenshot of console

--- assignment2/server.py
+++ assignment2/server.py
@@ -1,3 +1,9 @@
+# assignment 2 task4
+# Andrea Mildes - mildes@uni-koblenz.de
+# Sebastian Blei - sblei@uni-koblenz.de
+# Johannes Kircher - jkircher@uni-koblenz.de
+# Abdul Afghan - abdul.afghan@outlook.de
+
 import socket
 import json
 
