692fd26564f3954a369460706e5b237d21b155e7
Andrea Mildes <Andrea Mildes>
2016-11-22 17:12:25 +0100

    well, we do need the .py to include them

---
+++ workspace/a4/tex/downloadEverything.py
@@ -0,0 +1,63 @@
+# assignment 4 task 2
+# Andrea Mildes - mildes@uni-koblenz.de
+# Sebastian Blei - sblei@uni-koblenz.de
+# Johannes Kirchner - jkirchner@uni-koblenz.de
+# Abdul Afghan - abdul.afghan@outlook.de
+
+import sys
+import re
+import client
+from urllib.parse import urlparse
+
+#Parse filename and url
+def get_args():
+    filename = sys.argv[1]
+    url = sys.argv[2]
+    #print(args)
+    #args = 'http://west.uni-koblenz.de/sites/default/files/styles/personen_bild/public/_IMG0076-Bearbeitet_03.jpg'
+    #args = 'http://west.uni-koblenz.de/en/studying/courses/ws1617/introduction-to-web-science'
+    return filename, url
+
+#open the file
+def get_file(filename):
+    file = open(filename, 'br')
+    return file.read()
+
+#Get all image urls from the file
+def get_urls(data, url):
+    domain = urlparse(url)
+    pattern = re.compile('(<img .* \/>)')
+    images = pattern.findall(data.decode())
+
+    pattern2 = re.compile('(?<=src=")(.*?)(?=")')
+    links = []
+    for element in images:
+        links.append(pattern2.findall(element))
+
+    #Fix relative paths
+    for i in range(0, len(links)):
+        url_comps = urlparse(links[i][0])
+        if url_comps.netloc == "":
+            links[i] = domain.scheme + "://" + domain.netloc + links[i][0]
+        else:
+            links[i] = links[i][0]
+    return links
+
+
+#iterate over the list and call the function from task 01
+def download_images(links):
+    for i in range(0, len(links)):
+        print('\n ' + str(i) + '\n')
+        client.main_func(links[i])
+
+#call all functions
+def main_func():
+    filename, url = get_args()
+    data = get_file(filename)
+    links = get_urls(data, url)
+    download_images(links)
+    print(links)
+
+
+if __name__ == '__main__':
+    main_func()
