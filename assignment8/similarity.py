023b4bd3e376c6768f2fe133dd3df6274dcd7be9
johannes-k <jo@jo-kirchner.me>
2017-01-11 08:08:27 +0100

    Added final code

--- assignment8/similarity.py
+++ assignment8/similarity.py
@@ -17,6 +17,7 @@ np.set_printoptions(threshold=np.nan)
 document_freq = {}
 word_vector = {}
 article_dic = {}
+sparse_tfidf_vectors = {}
 df1 = pd.DataFrame()
 df2 = pd.DataFrame()
 
@@ -39,7 +40,7 @@ def write_file(filename, generated_string):
 
     t = str(round((time.time() - start_time), 2)).zfill(5)
     logging.info("[" + t + "] " +
-                 "Finished writing \"" + filename + "\" file. \n\n")
+                 "Finished writing \"" + filename + "\" file. \n")
     return
 
 
@@ -110,7 +111,7 @@ def log_doc_freq():
     #     print(str(v) + " | " + k)
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "]  Done! \n\n")
+    logging.info("[" + t + "]  Done! \n")
 
     return
 
@@ -155,7 +156,7 @@ def create_dic_of_all_article_with_terms():
         dic[i] = articles_dic
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "]  Done! \n\n")
+    logging.info("[" + t + "]  Done! \n")
 
     return dic
 
@@ -178,34 +179,62 @@ def create_word_vectors():
         i += 1
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "]  Done! \n\n")
+    logging.info("[" + t + "]  Done! \n")
+
+    return
+
+
+# Computes the sparse tfidf vector for an article and stores
+# it in a global dictionary together with its euclidean
+# length as a tuple
+# We don't do this in the calculateCosineSimilarity method
+# because we only want to compute the vector for every article once
+# since this improves performance dramatically.
+def compute_sparse_tfidf_vector(article_id):
+    global sparse_tfidf_vectors
+    a = article_dic[article_id]
+
+    # Calculate the vector for the given article
+    article_vector = np.zeros(len(document_freq))
+    for term, tfidf in a.items():
+        # Get the vector corresponding to the current word
+        vec = word_vector[term]
+        # Multiply vector with tfidf
+        article_vector += vec * tfidf
+
+    sparse_tfidf_vectors[article_id] = (article_vector, np.linalg.norm(article_vector))
+
+    return
+
+
+# Iterates of a list of article ids and computes its sparse vectors
+def compute_sparse_tfidf_vector_from_list(article_list):
+    t = str(round((time.time() - start_time), 2))
+    logging.info("[" + t + "]  Computing sparse vectors ...")
+
+    for i in range(0, len(article_list)):
+        compute_sparse_tfidf_vector(article_list[i])
+
+    t = str(round((time.time() - start_time), 2))
+    logging.info("[" + t + "]  Done!")
 
     return
 
 
 def calculateCosineSimilarity(tfIdfDict1, tfIdfDict2):
-    # Calculate the vector for tfIdfDict1
-    article_vector1 = np.zeros(len(document_freq))
-    for term, tfidf in tfIdfDict1.items():
-        # Get the vector corresponding to the current word
-        vec = word_vector[term]
-        # Multiply vector with tfidf
-        article_vector1 += vec * tfidf
-
-    # Calculate the vector for tfIdfDict2
-    article_vector2 = np.zeros(len(document_freq))
-    for term, tfidf in tfIdfDict2.items():
-        # Get the vector corresponding to the current word
-        vec = word_vector[term]
-        # Multiply vector with tfidf
-        article_vector2 += vec * tfidf
+    vector1 = tfIdfDict1[0]
+    vector2 = tfIdfDict2[0]
 
     # Get the dot product of the vectors
-    dot = article_vector1.dot(article_vector2)
+    dot = vector1.dot(vector2)
 
     # Get the length of both vectors
-    length_vector1 = np.linalg.norm(article_vector1)
-    length_vector2 = np.linalg.norm(article_vector2)
+    length_vector1 = tfIdfDict1[1]
+    length_vector2 = tfIdfDict2[1]
+
+    # Some articles are emtpy. If this is the case, mark it
+    if length_vector1 == 0 or length_vector2 == 0:
+        return -1
 
     # Inverse cosine of the dot product divided by the product of
     # the length of both vectors
@@ -257,17 +286,25 @@ def compute_jaccard_similarity_of_all_articles(articles):
     for i in range(0, len(articles)):
         i_article = create_set(df1.loc[articles[i]].text)
 
+        # Some articles are emtpy. If this is the case, skip it.
+        if len(i_article) == 0: continue
+
         for j in range(0, i):
             j_article = create_set(df1.loc[articles[j]].text)
+
+            # Some articles are emtpy. If this is the case, skip it.
+            if len(j_article) == 0: continue
 
             jaccard = calcJaccardSimilarity(i_article, j_article)
             matrix[i, j] = jaccard
 
+        # Logging
         if i % 10 == 0:
-            logging.info("i: " + str(i))
+            t = str(round((time.time() - start_time), 2))
+            logging.info("[" + t + "] Finished " + str(i) + "% of all articles")
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "]  Done! \n\n")
+    logging.info("[" + t + "]  Done! \n")
 
     return matrix
 
@@ -279,23 +316,65 @@ def compute_cosine_similarity_of_all_articles(articles):
 
     matrix = np.zeros(shape=(100, 100))
 
+    # First compute the sparse vectors for each article in the list
+    compute_sparse_tfidf_vector_from_list(articles)
+
     # Compute every possible combination of articles. We only need to
     # calculate the similarity once per pair and we do
     # not need to calculate the similarity of the article with itself.
     for i in range(0, len(articles)):
-        i_article = article_dic[articles[i]]
+        i_vec = sparse_tfidf_vectors[articles[i]]
 
         for j in range(0, i):
-            j_article = article_dic[articles[j]]
+            j_vec = sparse_tfidf_vectors[articles[j]]
 
-            cos_sim = calculateCosineSimilarity(i_article, j_article)
+            cos_sim = calculateCosineSimilarity(i_vec, j_vec)
+            # Store the cosine similarity in a matrix
             matrix[i, j] = cos_sim
 
+        # Logging
         if i % 10 == 0:
-            logging.info("i: " + str(i))
+            t = str(round((time.time() - start_time), 2))
+            logging.info("[" + t + "] Finished " + str(i) + "% of all articles")
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "]  Done! \n\n")
+    logging.info("[" + t + "]  Done! \n")
+
+    return matrix
+
+
+def compute_jaccard_similarity_for_outlinks_of_all_articles(articles):
+    t = str(round((time.time() - start_time), 2))
+    logging.info("[" + t + "]  Compute jaccard similarities for outlinks "
+                           "of all articles ...")
+
+    matrix = np.zeros(shape=(100, 100))
+
+    # Compute every possible combination of articles. We only need to
+    # calculate the similarity once per pair and we do
+    # not need to calculate the similarity of the article with itself.
+    for i in range(0, len(articles)):
+        i_article = set(df2.loc[articles[i]].out_links)
+
+        # Some articles are emtpy. If this is the case, skip it.
+        if len(i_article) == 0: continue
+
+        for j in range(0, i):
+            j_article = set(df2.loc[articles[j]].out_links)
+
+            # Some articles are emtpy. If this is the case, skip it.
+            if len(j_article) == 0: continue
+
+            jaccard = calcJaccardSimilarity(i_article, j_article)
+            matrix[i, j] = jaccard
+
+        # Logging
+        if i % 10 == 0:
+            t = str(round((time.time() - start_time), 2))
+            logging.info("[" + t + "] Finished " + str(i) + "% of all articles")
+
+    t = str(round((time.time() - start_time), 2))
+    logging.info("[" + t + "]  Done! \n")
 
     return matrix
 
@@ -308,8 +387,8 @@ def pretty_print_matrix(matrix):
 
 
 def main():
-    # Set start time
     global start_time, df1, df2, article_dic
+    # Set start time
     start_time = time.time()
 
     # Reset log file
@@ -317,7 +396,7 @@ def main():
         pass
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "] --- Started --- \n\n")
+    logging.info("[" + t + "] --- Started --- \n")
 
     store = pd.HDFStore('store2.h5')
     df1 = store['df1']
@@ -331,14 +410,16 @@ def main():
         str(df1[df1.name == 'Germany'].text.values[0]))
     word_set_europe = create_set(
         str(df1[df1.name == 'Europe'].text.values[0]))
-    print(calcJaccardSimilarity(word_set_germany, word_set_europe))
+
+    print("Jaccard Similarity of Germany and Europe: " +
+          str(calcJaccardSimilarity(word_set_germany, word_set_europe)))
 
     # ----------------------------------------------#
     # 1.1.2 Calculate the cosine similarity for the #
     # article "Germany" and "Europe"                #
     # ----------------------------------------------#
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "] --- Started calculations for 1.1.2 --- \n\n")
+    logging.info("[" + t + "] --- Started calculations for 1.1.2 ---")
 
     # Compute the document frequency and store it in
     # global dictionary document_freq
@@ -358,54 +439,84 @@ def main():
     ger_id = df1[df1.name == "Germany"].index[0]
     eur_id = df1[df1.name == "Europe"].index[0]
 
+    # Create sparse vectors for each article
+    compute_sparse_tfidf_vector_from_list([ger_id, eur_id])
+
     # Calculate the cosine similarity of both articles
-    cos_sim = calculateCosineSimilarity(article_dic[ger_id],
-                                        article_dic[eur_id])
-    print(cos_sim)
+    cos_sim = calculateCosineSimilarity(sparse_tfidf_vectors[ger_id],
+                                        sparse_tfidf_vectors[eur_id])
+
+    print("Cosine Similarity for Germany and Europe: " + str(cos_sim))
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "] --- Finished calculations for 1.1.2 --- \n\n")
+    logging.info("[" + t + "] --- Finished calculations for 1.1.2 --- \n")
 
     # --------------------------#
     # 1.2 Similarity of graphs  #
     # --------------------------#
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "] --- Started calculations for 1.2 --- \n\n")
+    logging.info("[" + t + "] --- Started calculations for 1.2 ---")
 
     germany_outlinks = set(df2[df2.name == "Germany"].out_links.values[0])
     europe_outlinks = set(df2[df2.name == "Europe"].out_links.values[0])
-    outlinks_jaccard_sim =  calcJaccardSimilarity(germany_outlinks,
+    outlinks_jaccard_sim = calcJaccardSimilarity(germany_outlinks,
                                                   europe_outlinks)
-    print(outlinks_jaccard_sim)
+
+    print("Jaccard Similarity of outlinks of Germany and Europe: "
+          + str(outlinks_jaccard_sim))
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "] --- Finished calculations for 1.2 --- \n\n")
+    logging.info("[" + t + "] --- Finished calculations for 1.2 --- \n")
 
     # --------------------------#
     # 1.4 Implement the measure #
     # --------------------------#
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "] --- Started calculations for 1.4 --- \n\n")
+    logging.info("[" + t + "] --- Started calculations for 1.4 ---")
 
+    t = str(round((time.time() - start_time), 2))
+    logging.info("[" + t + "] --- Started calculations "
+                           "for the 100 longest articles --- \n")
     # Find the 100 longest articles
     l_articles = get_longest_articles()
 
     jaccard_matrix = compute_jaccard_similarity_of_all_articles(l_articles)
+    print("\n\n ---------- JACCARD MATRIX ---------- \n\n")
     pretty_print_matrix(jaccard_matrix)
 
     cosine_matrix = compute_cosine_similarity_of_all_articles(l_articles)
+    print("\n\n ---------- COSINE MATRIX ---------- \n\n")
     pretty_print_matrix(cosine_matrix)
 
+    jaccard_outlinks_matrix = compute_jaccard_similarity_for_outlinks_of_all_articles(l_articles)
+    print("\n\n ---------- JACCARD MATRIX FOR OUTLINKS ---------- \n\n")
+    pretty_print_matrix(jaccard_outlinks_matrix)
+
+    t = str(round((time.time() - start_time), 2))
+    logging.info("[" + t + "] --- Started calculations "
+                           "for 100 random articles --- \n")
+
     # Find 100 random articles
-    #r_articles = get_random_articles()
-    #compute_similarity()
+    r_articles = get_random_articles()
+    jaccard_matrix = compute_jaccard_similarity_of_all_articles(r_articles)
+    print("\n\n ---------- JACCARD MATRIX ---------- \n\n")
+    pretty_print_matrix(jaccard_matrix)
+
+    cosine_matrix = compute_cosine_similarity_of_all_articles(r_articles)
+    print("\n\n ---------- COSINE MATRIX ---------- \n\n")
+    pretty_print_matrix(cosine_matrix)
+
+    jaccard_outlinks_matrix = compute_jaccard_similarity_for_outlinks_of_all_articles(r_articles)
+    print("\n\n ---------- JACCARD MATRIX FOR OUTLINKS ---------- \n\n")
+    pretty_print_matrix(jaccard_outlinks_matrix)
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "] --- Finished calculations for 1.4 --- \n\n")
+    logging.info("[" + t + "] --- Finished calculations for 1.4 --- \n")
 
     t = str(round((time.time() - start_time), 2))
-    logging.info("[" + t + "] --- Finished --- \n\n")
+    logging.info("[" + t + "] --- Finished ---")
 
+    exit(1)
 
 if __name__ == '__main__':
     main()
