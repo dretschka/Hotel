80c1e967f90eb6594c095fb74ebf85bf98c6e198
unknown <sb.cordy@gmail.com>
2017-01-24 21:37:55 +0100

    add a small correction to task 3

--- workspace/a9/diameter.py
+++ workspace/a9/diameter.py
@@ -66,7 +66,7 @@ def get_edges(name):
     return edges
 
 #Note: This program takes very long to terminate, since it has a complexity of O(V * (V * E)), where V is the number of Articles and E is the total number of outgoing links. The algorithm for calculating the shortest path uses the Breadth-First Search Algorithm. It may take several hours to calculate the diameter.     
-# That being said, since the longest shortest path possible can only be as large as the amount of articles - 1    
+# That being said, since the longest shortest path possible can only be as large as the amount of articles - 1, the diameter can not exceed that value 
 def main():
     global df1, df2, graph
     if(len(sys.argv) > 1):
