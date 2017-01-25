10c28ddcae64d8004cd5123e9c0e2d1d63bc0714
johannes-k <jo@jo-kirchner.me>
2017-01-25 07:07:04 +0100

    Added task 1 & 2

--- workspace/a9/diameter.py
+++ workspace/a9/diameter.py
@@ -1,12 +1,21 @@
+# assignment 9
+# Andrea Mildes - mildes@uni-koblenz.de
+# Sebastian Blei - sblei@uni-koblenz.de
+# Johannes Kirchner - jkirchner@uni-koblenz.de
+# Abdul Afghan - abdul.afghan@outlook.de
+
 import sys
 import pandas as pd
 import math
 from queue import *
 
-#this creates a dictionary with the article name as the node of the graph and its outgoing links as the connection between the nodes.
-#Note: the connections between the nodes are one-directional, because an outgoing link may appear in one article
+#this creates a dictionary with the article name as the node of
+# the graph and its outgoing links as the connection between the nodes.
+#Note: the connections between the nodes are one-directional,
+# because an outgoing link may appear in one article
 #but there may not be an outgoing link to the previous article.
-#The nodes are the keys and the lists of connections are the values in our dictionary.
+#The nodes are the keys and the lists of connections are the values
+# in our dictionary.
 
 def create_graph():
     graph = {}
@@ -19,9 +28,11 @@ def create_graph():
     
     return graph
 
-# This is the actual calculation of the shortest path from a given article to every other article
+# This is the actual calculation of the shortest path from a given
+# article to every other article
 # using all possible combinations of outgoing links
-# it returns a dictionary with the name of the destination article and the length of its path
+# it returns a dictionary with the name of the destination article
+# and the length of its path
     
 def shortest_path(start, edges):
     q = Queue()
@@ -44,8 +55,10 @@ def shortest_path(start, edges):
             pass
     return distance
 
-# This method gets the dictionary with all the shortest paths as an argument and simply has to iterate over it
-# in order to find the largest value. That largest value is the diameter of the graph    
+# This method gets the dictionary with all the shortest paths
+#  as an argument and simply has to iterate over it
+# in order to find the largest value. That largest value is
+# the diameter of the graph
     
 def get_diameter(dict):
     start = ''
@@ -58,15 +71,22 @@ def get_diameter(dict):
                 start = key
                 end = link
     return (diameter, start, end)
-#This method simply stores every possible edge (starting and end point) in the whole graph into a single list    
+#This method simply stores every possible edge
+# (starting and end point) in the whole graph into a single list
 def get_edges(name):
     edges = []
     for edge in graph.get(name):
         edges.append((name,edge))
     return edges
 
-#Note: This program takes very long to terminate, since it has a complexity of O(V * (V + E)), where V is the number of Articles and E is the total number of outgoing links. The algorithm for calculating the shortest path uses the Breadth-First Search Algorithm. It may take several hours to calculate the diameter.     
-# That being said, since the longest shortest path possible can only be as large as the amount of articles - 1, the diameter can not exceed that value 
+#Note: This program takes very long to terminate, since it has a
+# complexity of O(V * (V + E)), where V is the number of Articles and E is
+# the total number of outgoing links. The algorithm for calculating the
+# shortest
+#  path uses the Breadth-First Search Algorithm. It may take several
+# hours to calculate the diameter.
+# That being said, since the longest shortest path possible can only be as
+# large as the amount of articles - 1, the diameter can not exceed that value
 def main():
     global df1, df2, graph
     if(len(sys.argv) > 1):
@@ -86,7 +106,8 @@ def main():
     for name in df2.name:
         distance[name] = shortest_path(name, edges)
     (diam, start, end) = get_diameter(distance)
-    print('The diameter is: \nFrom: ' + start + '\nTo: ' + end +'\nValue: ' + str(diam))
+    print('The diameter is: \nFrom: ' + start + '\nTo: ' +
+          end +'\nValue: ' + str(diam))
 
     
     
